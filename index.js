614d0e1ea801459e1e8a4db1cccd007a935d271f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-12 22:55:29 +0900

    interfaceを継承したclass

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// Interface -> Class
+//interfaceをclassに実装する場合（この場合は、user classは score, showCore()を持っていないといけない）
 var User = /** @class */ (function () {
     function User(name) {
+        this.score = 0;
         this.name = name;
     }
     User.prototype.sayHi = function () {
         console.log("hi i am " + this.name);
+    };
+    User.prototype.showCore = function () {
+        console.log("score is " + this.score);
     };
     return User;
 }());
--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
+// Interface -> Class
 
-class User {
+interface GameUser {
+    score: number;
+    showCore(): void;
+}
+
+//interfaceをclassに実装する場合（この場合は、user classは score, showCore()を持っていないといけない）
+class User implements GameUser{
     name: string;
+    score: number = 0;
+
     constructor(name: string) {
         this.name = name;
     }
     sayHi(): void {
         console.log("hi i am " + this.name);
     }
+    showCore(): void {
+        console.log("score is " + this.score);
+    }
+
 }
 
 const user = new User("testMan");
