d842b5374e43418d31823c9481e35c523f4e042f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-12 23:24:54 +0900

    ジェネリクスに制約を与える方法

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Generics
-//同じような記述ばかり
-// const getStringArray = (value: string): string[] => {
-//     return [value, value, value];
-// }
+var MyData = /** @class */ (function () {
+    function MyData(value) {
+        this.value = value;
+    }
+    MyData.prototype.getArray = function () {
+        return [this.value, this.value, this.value];
+    };
+    return MyData;
+}());
 //
-// const getNumberArray = (value: number): number[] => {
-//     return [value, value, value];
-// }
-//型の箇所を「T」と書くのが慣習的
-var getArray = function (value) {
-    return [value, value, value];
-};
-console.log(getArray(3));
-console.log(getArray("hello"));
+// let v1 = new MyData<string>("hello");
+// console.log(v1.getArray());
+// let v2 = new MyData<number>(234);
+// console.log(v2.getArray());
+var v3 = new MyData({ a: 32, b: 40 });
+console.log(v3.getArray());
--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 // Generics
 
-class MyData<T> {
+interface Result {
+    a: number;
+    b: number;
+}
+class MyData<T extends Result> {
     constructor(public value: T) {
     }
     getArray(): T[] {
         return [this.value, this.value, this.value];
     }
 }
+//
+// let v1 = new MyData<string>("hello");
+// console.log(v1.getArray());
+// let v2 = new MyData<number>(234);
+// console.log(v2.getArray());
 
-let v1 = new MyData<string>("hello");
-console.log(v1.getArray());
-let v2 = new MyData<number>(234);
-console.log(v2.getArray());
+let v3 = new MyData<Result>({a: 32, b: 40});
+console.log(v3.getArray());
+
